34671f1055d063c3898662bd2b3b30f85753d145
Hieu Nghiem Le <hieunghiemle@Hieus-MacBook-Pro.local>
2024-04-11 15:35:33 +0700

    update

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,7 +3,14 @@ export default function Login() {
   return (
     <div className="auth__background">
       <div className="auth__form">
-          
+          <h2>Adminstrator</h2>
+          <form action="">
+            <label htmlFor="">Account </label>
+            <input type="text" /><br />
+            <label htmlFor="">Password </label>
+            <input type="text" /><br />
+            <button>Log in</button>
+          </form>
       </div>
     </div>
   )
